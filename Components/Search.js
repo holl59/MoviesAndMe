203142c9574a4950e96624fe17b364d642d64ac9
holl59 <holl59@free.fr>
2020-12-23 10:34:58 +0100

    Frame des Films

---
+++ Components/Search.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { StyleSheet, View, Button, TextInput, FlatList, Text } from 'react-native'
+import films from '../Helpers/filmsData'
+import FilmItem from './FilmItem'
+
+
+class Search extends React.Component {
+    render() {
+        return (
+            <View style={styles.main_container}>
+                <TextInput  style={styles.textinput} placeholder='Titre du film'/>
+                <Button style={{ height: 50}} title='Recherche' onPress={ () => {}} />
+                <FlatList
+                    data={films}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({item}) => <FilmItem/>}
+                />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    main_container: { 
+        marginTop: 20, 
+        flex: 1 
+    },
+    textinput: {
+        marginLeft: 5,
+        marginRight: 5,
+        height: 50,
+        borderColor: '#000000',
+        borderWidth: 1,
+        paddingLeft: 5
+      }
+})
+
+export default Search
